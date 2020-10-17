faf216619baa250c2f1108f508f215e118de898d
Moti Elmakies <motiphone2003@gmail.com>
2020-10-17 23:30:14 +0300

    init

---
+++ app.js
@@ -0,0 +1,4 @@
+const myNotes = require('./notes');
+const chalk = require('chalk');
+
+console.log(chalk.red.inverse.bold('Success'));
---
+++ utils.js
@@ -0,0 +1,4 @@
+const name = 'Moti';
+
+const person = { name: 'moti', job: 'developer' };
+module.exports = person;
